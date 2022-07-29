b4dca3ed7c1aead18627ad118ec66ed2c0a0897b
December G <dgarnetsmith@gmail.com>
2022-07-29 13:35:00 -0600

    Add dotenv and fetch

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const fetch = require("node-fetch");
+const app = express();
+// set port
+
+const PORT = 3001;
+
+app.use();
+
+app.listen(port, () => {
+  console.log(`Server is now listening on port ${PORT}`);
+});
