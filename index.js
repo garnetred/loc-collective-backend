9f7dba6281da3f93ffc81a2808aee8ec2141fa40
December G <dgarnetsmith@gmail.com>
2022-07-29 14:28:09 -0600

    Create working search request

--- index.js
+++ index.js
@@ -4,10 +4,55 @@ const fetch = require("node-fetch");
 const app = express();
 // set port
 
-const PORT = 3001;
+const port = 3001;
 
-app.use();
+dotenv.config();
+
+app.get("/api/search", async (req, res) => {
+  console.log("trying");
+  console.log(req.query, "req params");
+  const { term } = req.query;
+  const { location } = req.query;
+  console.log(term, "search term");
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+  };
+
+  const response = await fetch(
+    `https://api.yelp.com/v3/businesses/search?term=${term}&category=(hairstylists, US)&location=${location}&limit=50`,
+    {
+      method: "GET",
+      headers,
+    }
+  );
+
+  const data = await response.json();
+  console.log(data, "data");
+  res.send({ message: data });
+  //   res.send({ message: "ok" });
+  //   try {
+  //     // need to send network request and append api key
+  //     // then return the result to the front end
+  //     // need to create a new headers object?
+  //     // should append authorization header to new headers
+  //   } catch (error) {
+  //     console.error(error);
+  //     console.log("error");
+  //     res.send({ error });
+  //   }
+});
+
+app.get("/hi", (req, res) => {
+  console.log("at least it logged");
+  res.send("called");
+});
+
+app.get("/", (req, res) => {
+  res.send("GET Request Called");
+});
 
 app.listen(port, () => {
-  console.log(`Server is now listening on port ${PORT}`);
+  console.log(`Server is now listening on port ${port}`);
 });
