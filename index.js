264208ef982906e75a427e008039607ac5ada93e
December G <dgarnetsmith@gmail.com>
2023-10-31 00:28:57 -0500

    Switch back to commonjs

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import express from "express";
-import { config } from "dotenv";
-import fetch from "node-fetch";
+// import express from 'express';
+const express = require('express');
+const { config } = require('dotenv');
+// import { config } from 'dotenv';
+const fetch = require('node-fetch');
+// import fetch from 'node-fetch';
+const cors = require('cors');
 const app = express();
-import cors from "cors";
-import favicon from "serve-favicon";
-import path from 'path';
-const __dirname = path.resolve()
+const favicon = require('serve-favicon');
+
+// import cors from 'cors';
+// import favicon from 'serve-favicon';
+// import path from 'path';
+// const __dirname = path.resolve();
 // set port
 
-const port = process.env.NODE_ENV !== "production" ? 3001 : process.env.PORT;
+const port = process.env.NODE_ENV !== 'production' ? 3001 : process.env.PORT;
 
 config();
 
@@ -16,11 +22,11 @@ app.use(cors());
 app.use(favicon(__dirname + '/images/favicon.ico'));
 
 const headers = {
-  "Content-Type": "application/json",
+  'Content-Type': 'application/json',
   Authorization: `Bearer ${process.env.YELP_API_KEY}`,
 };
 
-app.get("/api/search", async (req, res) => {
+app.get('/api/search', async (req, res) => {
   try {
     const { term } = req.query;
     const { location } = req.query;
@@ -28,9 +34,9 @@ app.get("/api/search", async (req, res) => {
     const response = await fetch(
       `https://api.yelp.com/v3/businesses/search?term=${term}&category=(hairstylists, US)&location=${location}&limit=50`,
       {
-        method: "GET",
+        method: 'GET',
         headers,
-      }
+      },
     );
 
     const data = await response.json();
@@ -40,12 +46,12 @@ app.get("/api/search", async (req, res) => {
   }
 });
 
-app.get("/api/stylist/:id", async (req, res) => {
+app.get('/api/stylist/:id', async (req, res) => {
   try {
     const { id } = req.params;
 
     const response = await fetch(`https://api.yelp.com/v3/businesses/${id}`, {
-      method: "GET",
+      method: 'GET',
       headers,
     });
 
@@ -56,16 +62,16 @@ app.get("/api/stylist/:id", async (req, res) => {
   }
 });
 
-app.get("/api/reviews/:id", async (req, res) => {
+app.get('/api/reviews/:id', async (req, res) => {
   try {
     const { id } = req.params;
 
     const response = await fetch(
       `https://api.yelp.com/v3/businesses/${id}/reviews`,
       {
-        method: "GET",
+        method: 'GET',
         headers,
-      }
+      },
     );
     const data = await response.json();
     res.send({ data });
@@ -74,7 +80,7 @@ app.get("/api/reviews/:id", async (req, res) => {
   }
 });
 
-app.get("/api/reviews", async (req, res) => {});
+app.get('/api/reviews', async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server is now listening on port ${port}`);
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
-const express = require('express');
+// const express = require('express');
+const app = require('../index');
 const nock = require('nock');
+const mockSearchData = require('../mocks/searchData.json');
 // const jest = require('jest');
 
-const app = express();
+// const app = express();
 
 describe('GET search results', () => {
   it('does a thing', () => {
@@ -11,11 +13,13 @@ describe('GET search results', () => {
   });
 
   it('should get search results based on a term and location', async () => {
-    const res = await request(app).get(
-      'api/search?term=interlocks+sisterlocks&location=boston',
-    );
-
+    const term = 'interlocks+sisterlocks';
+    const location = 'boston';
+    nock('https://api.yelp.com')
+      .get(`/v3/businesses/search?term=${term}&location=${location}`)
+      .reply(200, mockSearchData);
+    const res = await request(app).get(`/api/search`);
+    expect(res.status).toBe(200);
     console.log(res);
-    expect(res.headers).toHaveProperty('Content-Type');
   });
 });
