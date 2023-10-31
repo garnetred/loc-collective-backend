1e1f74c91b12ba469ed64f7c702bd211aba65805
December G <dgarnetsmith@gmail.com>
2023-10-30 23:57:33 -0500

    Install nock, create mock folder, update api test url

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,11 +1,9 @@
 const request = require('supertest');
 const express = require('express');
+const nock = require('nock');
 // const jest = require('jest');
 
-jest.mock('node-fetch');
-
 const app = express();
-const fetch = require('node-fetch');
 
 describe('GET search results', () => {
   it('does a thing', () => {
@@ -14,28 +12,10 @@ describe('GET search results', () => {
 
   it('should get search results based on a term and location', async () => {
     const res = await request(app).get(
-      'api/search?term="interlocks"&location="boston"',
+      'api/search?term=interlocks+sisterlocks&location=boston',
     );
 
     console.log(res);
     expect(res.headers).toHaveProperty('Content-Type');
-
-    //   .expect(request.headers)
-    //   .toBe({ 'Content-Type': 'application/json' })
-    //   .end(function (err, res) {
-    //     if (err) {
-    //       throw err;
-    //     } else {
-    //       console.log(res, 'res');
-    //     }
-    //   });
   });
-  //   it('should grab dog breeds', () => {
-  //     request('https://dog.ceo')
-  //       .get('/api/breeds/image/random')
-  //       .expect(200)
-  //       .end(function (err, res) {
-  //         if (err) throw err;
-  //       });
-  //   });
 });
