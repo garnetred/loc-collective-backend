3739a80b536ec9f188a41da674c0a0ace3255f23
December G <dgarnetsmith@gmail.com>
2023-10-30 23:40:27 -0500

    Begin testing get api/search

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,36 @@
-describe('test', () => {
-    it('does a thing', () => {
-        expect(true).toBe(true);
-    })
-})
+const request = require('supertest');
+const express = require('express');
+const fetch = require('node-fetch');
+// const jest = require('jest');
+
+// jest.mock('node-fetch');
+
+// import fetch from 'node-fetch';
+
+const app = express();
+
+describe('GET search results', () => {
+  it('does a thing', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should get search results based on a term and location', async () => {
+    await request(app)
+      .get('api/search?term="interlocks"&location-"boston"')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+      });
+    console.log(res);
+  });
+  //   it('should grab dog breeds', () => {
+  //     request('https://dog.ceo')
+  //       .get('/api/breeds/image/random')
+  //       .expect(200)
+  //       .end(function (err, res) {
+  //         if (err) throw err;
+  //       });
+  //   });
+});
